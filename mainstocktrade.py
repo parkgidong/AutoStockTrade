18cf82ecbc3f16e6889f07287dfc42463de151a2
parkgidong <77328115+parkgidong@users.noreply.github.com>
2021-03-16 08:50:46 +0900

    Update mainstocktrade.py

--- mainstocktrade.py
+++ mainstocktrade.py
@@ -7,7 +7,7 @@ from slacker import Slacker
 import time, calendar
 
 
-slack = Slacker('xoxb-1730506155696-1730528656272-gNRkeb69MmhnKTBrAzhF5tTe')
+slack = Slacker('')
 def dbgout(message):
     """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
